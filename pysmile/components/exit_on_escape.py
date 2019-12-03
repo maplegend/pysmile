f5032ec6b9fef8fd10a20e68b885c36710aa2996
yaroslav <maplegend@mail.ru>
2019-12-03 12:22:21 +0300

    Got rid of sys.exit()

--- pysmile/components/exit_on_escape.py
+++ pysmile/components/exit_on_escape.py
@@ -1,9 +1,8 @@
 import pygame
-import sys
 from ..component import Component
 from ..events.key_pressed import KeyPressedEvent
 
 
 class ExitOnEscape(Component):
     def applied_on_entity(self, entity):
-        entity.event_manager.bind(KeyPressedEvent, lambda event: sys.exit() if event.key == pygame.K_ESCAPE else None)
+        entity.event_manager.bind(KeyPressedEvent, lambda event: entity.scene.game.exit() if event.key == pygame.K_ESCAPE else None)
--- pysmile/components/game/game_event_handler.py
+++ pysmile/components/game/game_event_handler.py
@@ -1,5 +1,4 @@
 import pygame
-import sys
 from pysmile.game_component import GameComponent
 from pysmile.events.key_press import KeyPressEvent
 from pysmile.events.pygame_event import PyGameEvent
@@ -19,6 +18,6 @@ class GameEventHandlerComponent(GameComponent):
         for event in pygame.event.get():
             em.trigger_event(PyGameEvent(event))
             if event.type == pygame.QUIT:
-                sys.exit()
+                self.game.exit()
             if event.type == pygame.KEYDOWN:
                 em.trigger_event(KeyPressEvent(event.key))
